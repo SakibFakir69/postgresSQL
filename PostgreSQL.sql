c7775cca5d8d3e8ad01b0398696025abb8bf5a28
Sakib Fakir <FakirSakib22232@gmail.com>
2025-05-25 17:52:10 +0600

    1 problem done

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -1,9 +1,20 @@
 -- Active: 1747400426333@@127.0.0.1@5432@conservation_db@public
 
-
 SELECT * from rangers;
 
+SELECT * from species;
 
+SELECT * from sightings
+
+
+
+
+
+-- problem 1 
+
+INSERT INTO rangers(name , region)
+VALUES
+('Derek Fox','Coastal Plains');
 
 
 
